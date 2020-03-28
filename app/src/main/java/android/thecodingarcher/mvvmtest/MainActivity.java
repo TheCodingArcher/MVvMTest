693447a13f8cd81a5d9bd5ddf41953d42b712d86
TheCodingArcher <rick_thegladiator@yahoo.com>
2020-03-28 20:58:34 +0530

    Added Adapter, Model and layout files

--- app/src/main/java/android/thecodingarcher/mvvmtest/MainActivity.java
+++ app/src/main/java/android/thecodingarcher/mvvmtest/MainActivity.java
@@ -1,13 +1,52 @@
 package android.thecodingarcher.mvvmtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.thecodingarcher.mvvmtest.adapters.RecyclerAdapter;
+import android.thecodingarcher.mvvmtest.models.NicePlace;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private FloatingActionButton mFab;
+    private RecyclerView mRecyclerView;
+    private RecyclerAdapter mAdapter;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFab = findViewById(R.id.fab);
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        mAdapter = new RecyclerAdapter(this, new ArrayList<NicePlace>());
+        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void showProgressBar(){
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar(){
+        mProgressBar.setVisibility(View.GONE);
     }
 }
